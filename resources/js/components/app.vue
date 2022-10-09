b022d648418ad019e8e419633ab9b6869d62da25
cnabilgit <46906074+cnabilhub@users.noreply.github.com>
2022-10-09 16:16:42 +0100

    installing taiwind and vue js

---
+++ resources/js/components/app.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <div
+      class="bg-blue-100 rounded-lg py-5 px-6 mb-4 text-base text-blue-700 mb-3"
+      role="alert"
+    >
+      A simple primary alert - check it out!
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    console.log("Component mounted.");
+  },
+};
+</script>
